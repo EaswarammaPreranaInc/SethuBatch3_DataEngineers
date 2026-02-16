b33d9a139276bd1bb6e6286a309db0fa614eee6b
Kedhar66 <kedhardurgam@gmail.com>
2026-02-16 21:03:22 +0530

    10-02-2026 Home Work

---
+++ submissions/Durgam_Kedharnath_20260210.py
@@ -0,0 +1,82 @@
+ # float  object  demo  program (Home  work)
+a = 10.8
+print(a) #10.8
+print(type(a)) # <class 'float'>
+print(id(a)) # it store reference address of the float object
+b = 25.
+print(b) #25.0
+print(type(b)) # <class 'float'>
+c = .689
+print(c) #0.689
+d = 3.4E2
+print(d)
+print(type(d)) # <class 'float'>
+e = 9.62e-2
+print(e)
+print(9.8.2) # Error
+# complex object demo program
+a = 3 + 4j
+print(a) # 3+4j
+print(type(a)) #  <class 'complex'>
+print(id(a)) # it stores the reference address of the complex object
+print(a . real) # 3.0
+print(a . imag) # 4.0
+print(type(a . real))  # <class 'float'>
+print(type(a . imag))  # <class 'float'>
+# Find outputs (Home work)
+a = 6j
+print(a) # 6j
+print(type(a)) # <class 'complex'>
+print(a.real) # 0.0
+print(a.imag) #6.0
+print(5 + j6)  #Error In complex number j should be after imag number
+print(3 + 4i)  #Error In complex number j should be there instead of i
+print(4+j)  #Error in complex number there is no imag number
+print(4 + 1j) # 4+1j
+print(4 + 0j) # 4+0j
+# bool object demo program  (Home  work)
+a = True
+print(a) # True
+print(type(a)) # <class 'bool'>
+print(id(a)) # it stores the reference address of the bool object
+b = False
+print(b) # False
+print(type(b)) # <class 'bool'>
+print(True + True) # 2
+print(True + False) # 1
+print(False + True) # 1
+print(False + False) # 0
+print(True + True + True) # 3
+print(25 + 10.8 + True) # 36.8
+print(True > False)  # True
+print(True)# True
+print(False) # False
+print(true) #Error keywords should be capital T
+print(false)#Error keywords should be capital F
+ # Find  outputs (Home  work)
+a = 0O6247
+print(a)
+print(type(a))
+print(id(a))  
+b = 0o6247
+print(id(b))
+print(b)
+c = 3239
+print(c) # <class 'int'>
+print(id(c)) # it stores the reference address of the int object
+print(0o9248)
+ # Find  outputs  (Home  work)
+a = 0XA7B9
+print(a) 
+print(type(a))
+b = 0xBEEF
+print(b) 
+print(A7B9)  
+print('A7B9') # A7B9
+print(0XBEER)
+print(0XHYD)
+print(0xA7G9B)
+ # Find outputs (Home  work)
+a = 9248
+print(a) # 9248
+print(type(a)) # <class 'int'>
