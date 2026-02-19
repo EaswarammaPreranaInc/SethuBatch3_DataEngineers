838d00cb0f04f3d0f0e79d6e8745ac2200b6113e
Mangali Arun Kumar <168582862+arunkumarsn1729@users.noreply.github.com>
2026-02-19 11:26:12 +0530

    Update MangaliArun_Kumar_20260218.py

--- submissions/MangaliArun_Kumar_20260218.py
+++ submissions/MangaliArun_Kumar_20260218.py
@@ -18,7 +18,7 @@ print(id(b)) # Address of obejct False
 c = None
 print(id(c)) # Address of obejct None
 c = None
-print(id(c)) # same
+print(id(c)) # same address
 
 #  Find  outputs  (Home  work)
 a = 'Hyd'
@@ -28,14 +28,14 @@ a = 'Sec'
 print(id(a)) # Address of obejct Sec
 b = (10 , 20 , 15 , 18)
 print(id(b)) # Address of obejct b
-b[2] = 19
+b[2] = 19 #Error
 b = (30 , 40 , 35 , 32)
-print(id(b)) # Address of obejct b after change
+print(id(b)) # Address of obejct b 
 c = range(5)
 print(id(c)) # Address of obejct c
 c[3] = 10
 c = range(5)
-print(id(c)) # Address of obejct c after change
+print(id(c)) # Address of obejct second range
 
 # Find  outputs  (Home  work)
 a = 25
@@ -49,27 +49,27 @@ f = False
 print(e  is  f) # True
 g = range(10)
 h = range(10)
-print(g  is  h) # True
+print(g  is  h) # False
 
 #Find  outputs(Home  work)
 a = [10 , 20 , 15 , 18]
 b = [10 , 20 , 15 , 18]
-print(a  is  b) # True
+print(a  is  b) # False
 c =  {10 : 20, 30 : 40}
 d =  {10 : 20, 30 : 40}
-print(c  is  d)  # Error
+print(c  is  d)  # False
 e = (10 , 20 , 15 , 18)
 f = (10 , 20 , 15 , 18)
 print(e  is  f)  # True
 g = {10 , 20 , 15 , 18}
 h = {10 , 20 , 15 , 18}
-print(g  is  h)  # Error
+print(g  is  h)  # False
 
 # Find outputs (Home work)
 print(10 + 20) # 30
 print(10.8 + 20.6) # 31.4
 print(3 + 4j + 5 + 6j) # 7 + 10j
-print(True + False) # False
+print(True + False) # 1
 print('Hyder' + 'abad') # 'Hyderabad'
 print('Sankar' + 'Dayal' + 'Sarma') # 'SankarDayalSarma'
 print('10' + '20') # 1020
@@ -77,14 +77,14 @@ print([10 , 20 , 30] + [1 , 2 , 3]) # [10,20,30,1,2,3]
 print((25 , 10.8 , 'Hyd') + (3 + 4j , True , None)) # (25,10,8,'Hyd',3+4j,True,None)
 print({10 , 20} + {30 , 40}) # Error bcz dict is immutable
 print({10 : 'Hyd'} + {20 : 'Sec'}) # Error bcz dict is immutable
-print(range(4) + range(5)) #(0,1,2,3,0,1,2,3,4)
+print(range(4) + range(5)) #(0,1,2,3,0,1,2,3,4) Error
 print(10 + '20') # Error bcz sequence and non-sequence cannot concatinate
 print([10 , 20 , 30] + 5) # Error bcz sequence and non-sequence cannot concatinate
-print([10 , 20 , 30] + (40 , 50 , 60)) #Error bcz mutable and unmutable cannot concatinate
+print([10 , 20 , 30] + (40 , 50 , 60)) #Error bcz mutable and immutable cannot concatinate
 
 # Find outputs (Home work)
 print(25 * 3) # 75
-print(10.8 * 25.6) # varies
+print(10.8 * 25.6) # varies 276.48
 print(True * False) # False
 print((3 + 4j) * (5 + 6j)) #error 
 print(3 + 4j * 5 + 6j) #error
