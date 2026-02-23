c12b7f5be1fe395dd7a0dc4577d810c2e9674729
Mangali Arun Kumar <168582862+arunkumarsn1729@users.noreply.github.com>
2026-02-23 12:16:40 +0530

    Update MangaliArun_Kumar_20260221.py

--- submissions/MangaliArun_Kumar_20260221.py
+++ submissions/MangaliArun_Kumar_20260221.py
@@ -1,5 +1,5 @@
 #  abs()  function  demo  program
-from  builtins  import  abs
+from  builtins  import  abs #optional
 print(abs(-35.8)) # 35.8
 print(abs(-27)) # 27
 print(abs(29.5)) # 29.5
@@ -12,31 +12,31 @@ from  builtins  import   max , min
 print(max(10.8 , 20.6)) # 20.6
 print(min(10.8 , 20.6 , 5.9 , 12.3)) # 5.9
 print(max(25 , 10.8)) # 25
-import  builtins
+import  builtins #bmandatory
 print(builtins . max(10 , 20 , 30)) # 30
 print(builtins . min(10 , 20 , 15 , 5 , 12)) # 5
 
 # pow()  function  demo  program
-from  builtins  import  pow
+from  builtins  import  pow #optional
 print(pow(10 , -2)) # 0.01
-print(pow(4 , pow(3 , 2))) # 42122
-import  builtins
+print(pow(4 , pow(3 , 2))) # 262144
+import  builtins # mandatory
 print(builtins . pow(2 , 3)) # 8
-print(builtins . pow(-2 , -3)) # 8
+print(builtins . pow(-2 , -3)) # -0.125
 
 # Find  outputs
-from builtins import kwlist #  to  import   kw  list
+from keyword import kwlist #  to  import   kw  list
 print(kwlist) #  to  print  kwlist  i.e.  [and , or , not , is , in , None , True , False , .....]
-print(keyword . kwlist) #  to  print  number  of  keywords  i.e.  35
-print(type(keyword)) #  to  print  type  of kwlist  i.e.  <class 'list'>
-print(keyword . kwlist) # [and , or , not , is , in , None , True , False , .....] 
+print(len(kwlist)) #  to  print  number  of  keywords  i.e.  35
+print(type(kwlist)) #  to  print  type  of kwlist  i.e.  <class 'list'>
+#print(keyword . kwlist) # error bcz we didn't imported keyword module 
 
 #  Find  outputs  (Home  work)
-from builtins import keyword # to  import   keyword  module
-print(kwlist) # to  print  kwlist
-#print()  to  print  number  of  keywords
-#How  to  print  type  of kwlist
-print(kwlist)
+import keyword # to  import   keyword  module
+print(keyword.kwlist) # to  print  kwlist
+print(len(keyword.kwlist)) # to  print  number  of  keywords
+print(type(keyword.kwlist)  # to  print  type  of kwlist
+#print(kwlist) error kwlist not imported
 
 # How  to  read  complex  input ?
 x = complex(input('Enter  complex  number  :  ')) # 3+4j
@@ -45,28 +45,29 @@ print(x) # 3+4j
 
 #  Tricky  program
 # Find  outputs  (Home  work)
-print(eval("    'hyd'   ")) # hyd
+print(eval("    'hyd'   ")) # 'hyd'
 hyd = 'Sec'
-print(eval('hyd')) # Sec
+print(eval('hyd')) # 'Sec'
 sec = '25'
-print(eval('sec')) # 25
-print(eval(sec)) # 25
+print(eval('sec')) # '25'
+print(eval(sec)) # 25 converts string object to respective object
 cyb = 10.8
 print(eval('cyb')) # 10.8
-print(eval(cyb)) # 10.8
+#print(eval(cyb)) # Error bcz argument has to be in string
 
 #  Most  tricky  program
 #  Find  output  (Home  work)
-print(eval('print("Hyd")'))
+print(eval('print("Hyd")')) # Hyd 
+                            # None
 
 #  Find  outputs  (Home  work)
 print(bool('False')) # True
-print(eval('False')) # Error
+print(eval('False')) # False
 print(bool('')) # False
 print(eval('  ""  ')) # Empty string
-print(eval('')) # Error
-print(eval('  " "   ')) # Empty string
-print(eval(' ')) # empty string
+#print(eval('')) # Error
+print(eval('  " "   ')) # space
+#print(eval(' ')) # Error
 
 # What  is  the  advantage  of  eval(input()) ?
 x = eval(input('Enter  any  input  :  ')) # 2
@@ -77,6 +78,6 @@ print(x) # 2
 a = input('Enter  any  string  :  ') # hyd
 print(len(a)) # 3
 print(a) # hyd
-b = eval(input('Enter   any  string  : ')) # this is better way,# sec
+b = eval(input('Enter   any  string  : ')) # this isn't better way,# "sec"
 print(len(b)) # 3
 print(b) # sec
