77d3203ef98518e7199daedda99f59e00409d4ee
Akhil-11001 <edapuakhil11001@gmail.com>
2026-03-03 18:32:16 +0530

    Add files via upload
    python_home_work_03

---
+++ submissions/Akhil_edapu_20260303.py
@@ -0,0 +1,164 @@
+'''
+Write  a  program  to  search  for  'x'  in  fibonacci  series
+
+1) Let  input  be   10
+    What  is  the  output ? --->	Not  found
+
+2) Let  input  be   21
+    What  is  the  output ? --->  Found
+
+3) Do  not  generate  fibonacci  series
+
+Write  a  program  to  search  for  'x'  in  fibonacci  series
+
+1) Let  input  be   10
+    What  is  the  output ? --->	Not  found
+
+2) Let  input  be   21
+    What  is  the  output ? --->  Found
+
+3) Do  not  generate  fibonacci  series
+'''
+
+n = int(input("Enter a number:"))
+key = num
+
+a=0
+b=1
+
+if n>0:
+    print(a)
+    print(b)
+    c=a+b
+    while(c<=key):
+        print(c)
+        a=b
+        b=c
+        c=a+b
+        if key == c:
+            print('found')
+    else:
+        print('not found')
+
+
+
+'''
+Modify  following   program  with  walrus  operator
+
+Hint:  Combine  lines  8   and   9  to a  single  line  with   walrus  operator
+'''
+try:
+	sum =  ctr = 0
+	while  True:
+		x = eval(input('Enter input  (ctrl + z  to  stop)  :  '))
+		sum += x
+		ctr +=1
+except  EOFError:
+	try:
+		print(F'Average :   {sum / ctr}')
+	except  ZeroDivisionError:
+		print('Enter  at  least  one  input')
+except  (NameError , TypeError):
+	print('Input  can  not  be  string')
+
+
+'''
+Write  a  program  to  determine  command  line  input  is  even  number  or  odd  number
+
+1) py  prog3.py  26
+    What  is  the  output ?  ---> Even  number
+
+2) py  prog3.py  45
+    What  is  the  output ?  ---> Odd  number
+
+3) py  prog3.py
+    What  is  the  output  ?  --->  Pls  send  an  integer  input
+
+4) py  prog3.py  10.8
+    What  is  the  output ?  ---> Pls  send   an  integer  input
+
+5) py  prog3.py  Ten
+    What  is  the  output  ?  ---> Pls  send   an  integer  input
+'''
+
+import sys 
+
+try:
+    num = int(sys.argv[1])
+    if num %2 ==0:
+        print('Even number')
+    else:
+        print('Odd number')
+except:
+    print('Enter only integer')
+
+
+'''
+Write  a  program  to  determine  average  of  command  line  inputs
+
+1) py   prog4.py   10.8   25   True   14.6   19   False   7.4
+    What  is  argv ?  --->  ['prog4.py' , '10.8' , '25' , 'True' , '14.6' , '19' , 'False' , '7.4']
+    What  is  list  'a'  ?  ---> 	[10.8 , 25 , True , 14.6 , 19 , False , 7.4]
+	How  to  determine  sum  of  list  elements ?  ---> sum(a)
+    How  to  determine  number  of  list  elements ?  ---> len(a)
+
+2) py   prog4.py
+    What  is  the  output ?  --->  Pls  send  number  inputs
+
+3) py   prog4.py  25   'Ten'
+    What  is  the  output  ?  --->  Pls  send  number  inputs
+'''
+
+
+import sys
+
+a=[]
+sum=0
+num = eval(input("Enter a list :"))
+for i in (sys.argv[1:]):
+    a.append(i)
+
+print(f'sum={sum(a)}')
+print(f'length={len(a)}')
+
+
+
+
+'''
+Write  a  program  to  sort  command  line  inputs  in  ascending  order  and  descending  order
+
+1) py  prog5.py  10   20    15.8   5   12.6
+    What  is  argv ?  --->  ['prog5.py' , '10' , '20' , '15.8' , '5' , '12.6']
+    What  is  list  'a' ?  --->  [10 , 20 , 15.8 , 5 , 12.6]
+    How  to  sort  list  'a' ?  ---> sorted(a)
+    How  to  sort  list  'a'  in  descending  order  ?  ---> sorted(a , reverse = True)
+
+2) py  prog5.py   25   'Ten'
+    What  is  the  output ?  ---> Do  not  send  number  and  string  
+	
+3) py  prog5.py  
+    What  is  the  output ?  --->  Pls  send  inputs
+	
+3) py  prog5.py    'Hyd'  'Sec'  'Cyb'
+    What  are  the  outputs ?  --->   ['Cyb' , 'Hyd' , 'Sec']
+													   ['Sec' , 'Hyd' , 'Cyb']
+'''
+
+
+import sys
+a=[]
+num = eval(input("Enter anything :"))
+for i in (sys.argv[1:]):
+    a.append(i)
+
+
+print(f'sorted={sorted(a)}')
+print(f'Descending ={sorted(a,reverse =True}')
+
+
+
+
+
+
+
+    
