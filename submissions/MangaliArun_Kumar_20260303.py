af1dc66724a08c3b8cd0ebdbe085a9805e71c4fc
Mangali Arun Kumar <168582862+arunkumarsn1729@users.noreply.github.com>
2026-03-08 01:15:07 +0530

    MangaliArun_Kumar_20260303.py

--- submissions/MangaliArun_Kumar_20260303.py
+++ submissions/MangaliArun_Kumar_20260303.py
@@ -1,8 +1,5 @@
-'''
-Modify  following   program  with  walrus  operator
+# Modify  following   program  with  walrus  operator
 
-Hint:  Combine  lines  8   and   9  to a  single  line  with   walrus  operator
-'''
 try:
 	sum =  ctr = 0
 	while  x := eval(input('Enter input  (ctrl + z  to  stop)  :  ')):
@@ -16,57 +13,49 @@ except  EOFError:
 except  (NameError , TypeError):
 	print('Input  can  not  be  string')
 
-'''
-Write  a  program  to  determine  command  line  input  is  even  number  or  odd  number
+# Write  a  program  to  determine  command  line  input  is  even  number  or  odd  number
 
-1) py  prog3.py  26
-    What  is  the  output ?  ---> Even  number
-
-2) py  prog3.py  45
-    What  is  the  output ?  ---> Odd  number
-
-3) py  prog3.py
-    What  is  the  output  ?  --->  Pls  send  an  integer  input
-
-4) py  prog3.py  10.8
-    What  is  the  output ?  ---> Pls  send   an  integer  input
-
-5) py  prog3.py  Ten
-    What  is  the  output  ?  ---> Pls  send   an  integer  input
-'''
-
-'''
-Write  a  program  to  determine  average  of  command  line  inputs
-
-1) py   prog4.py   10.8   25   True   14.6   19   False   7.4
-    What  is  argv ?  --->  ['prog4.py' , '10.8' , '25' , 'True' , '14.6' , '19' , 'False' , '7.4']
-    What  is  list  'a'  ?  ---> 	[10.8 , 25 , True , 14.6 , 19 , False , 7.4]
-	How  to  determine  sum  of  list  elements ?  ---> sum(a)
-    How  to  determine  number  of  list  elements ?  ---> len(a)
-
-2) py   prog4.py
-    What  is  the  output ?  --->  Pls  send  number  inputs
-
-3) py   prog4.py  25   'Ten'
-    What  is  the  output  ?  --->  Pls  send  number  inputs
-'''
-
-'''
-Write  a  program  to  sort  command  line  inputs  in  ascending  order  and  descending  order
-
-1) py  prog5.py  10   20    15.8   5   12.6
-    What  is  argv ?  --->  ['prog5.py' , '10' , '20' , '15.8' , '5' , '12.6']
-    What  is  list  'a' ?  --->  [10 , 20 , 15.8 , 5 , 12.6]
-    How  to  sort  list  'a' ?  ---> sorted(a)
-    How  to  sort  list  'a'  in  descending  order  ?  ---> sorted(a , reverse = True)
-
-2) py  prog5.py   25   'Ten'
-    What  is  the  output ?  ---> Do  not  send  number  and  string  
+from sys import argv
+try:
+	for i in range(1,len(argv)):
+		num = int(argv[i])
+		if num != 0:
+		    if num % 2 == 0:
+			    print("Even number")
+		    else:
+			    print("Odd number")
+	    else:
+			print("Pls send a positive num")
+except:
+	print("Pls send an integer input")
 	
-3) py  prog5.py  
-    What  is  the  output ?  --->  Pls  send  inputs
-	
-3) py  prog5.py    'Hyd'  'Sec'  'Cyb'
-    What  are  the  outputs ?  --->   ['Cyb' , 'Hyd' , 'Sec']
-													   ['Sec' , 'Hyd' , 'Cyb']
-'''
+# Write  a  program  to  determine  average  of  command  line  inputs
+
+from sys import argv
+ttl = 0
+try:
+	for i in range(1,len(argv)):
+		num = eval(argv[i])
+		ttl += num
+	avg = ttl/(len(argv)-1)
+	print(F"average :{avg}")
+except:
+	print("Pls send an integer input")
+
+# Write  a  program  to  sort  command  line  inputs  in  ascending  order  and  descending  order
+
+from sys import argv
+try:
+    if len(argv) == 1:
+        print("Pls send inputs")
+    else:
+        a = []
+        for i in argv[1:]:
+            a.append(eval(i))
+        if all(type(x) == type(a[0]) for x in a):
+            print(sorted(a))
+            print(sorted(a, reverse=True))
+        else:
+            print("Do not send number and string")
+except:
+    print("Do not send number and string")
