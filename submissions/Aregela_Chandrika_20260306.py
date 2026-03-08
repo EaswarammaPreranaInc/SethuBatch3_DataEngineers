6cb38aa572bce574d1320f04c3fce087c4f92665
Aregela Naga Chandrika <131333970+ChandrikaAregela@users.noreply.github.com>
2026-03-08 22:55:23 +0530

    Add files via upload

--- submissions/Aregela_Chandrika_20260306.py
+++ submissions/Aregela_Chandrika_20260306.py
@@ -501,4 +501,4 @@ print(a . count('was'))  # 0
 a = 'Hyd is\tgreen\ncity.Hyd is\thitec\ncity.Hyd is\this\ncity'
 print(a . count(' ')) # 3 
 print(a . count('\t')) # 3 
-print(a . count('\n')) # 3
+print(a . count('\n')) # 3 
