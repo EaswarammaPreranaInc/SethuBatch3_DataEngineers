50b30566c45b6ede5de46a92757cab9947431c64
Shirisha <lovelysirii19@gmail.com>
2026-03-17 17:37:19 +0530

    Update SALENDRI_SHIRISHA_20260313.py

--- submissions/SALENDRI_SHIRISHA_20260313.py
+++ submissions/SALENDRI_SHIRISHA_20260313.py
@@ -254,6 +254,12 @@ Write  a  program  to  convert  a  nested  list  to  list  without  comprehensio
 
 Let  input  be  [ [10 , 20] , [30 , 40 , 50] , [60 , 70 , 80 , 90]]
 What  is  the  output ?  --->  [10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90]
+
+a = eval(input('Enter a list:'))
+b = []
+for x in a:
+    b.extend(x)
+print(b)
 '''
 
 
@@ -263,6 +269,10 @@ Write  a  program  to  convert  a  nested  list  to  list  with  comprehension
 Let  input  be  [ [10 , 20] , [30 , 40 , 50] , [60 , 70 , 80 , 90]]
 
 What  is  the  output ?  --->  [10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90]
+
+a = eval(input('Enter a list:'))
+b = [x extend(x) for x in a ]
+print(b)
 '''
 
 
