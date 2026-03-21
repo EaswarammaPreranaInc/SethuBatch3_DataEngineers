e7b118df1e0080f12e67daa34f62550ae2bb5c63
KeerthiThoudoju10 <tkeerthi700@gmail.com>
2026-03-21 11:18:57 +0530

    Update Thoudoju_Keerthi_20260320.py

--- submissions/Thoudoju_Keerthi_20260320.py
+++ submissions/Thoudoju_Keerthi_20260320.py
@@ -68,9 +68,9 @@ print(b) #[5, 10, 15, 18, 20]
 c = sorted(a . values())
 print(c) #['Blue' , 'Green', 'Red','White', 'Yellow'}
 d = sorted(a . items())
-print(d)#[5 : 'White',10 : 'Red' ,  15 : 'Blue', 18 : 'Yellow' ,20 : 'Green'   ]
+print(d)#[(5,'White'),(10 ,'Red'),  (15,'Blue'), (18,'Yellow') ,(20,'Green')   ]
 f  = sorted(a  , reverse = True)
-print(f) #[20 : 'Green', 18 : 'Yellow' , 15 : 'Blue', 10 : 'Red' , 5 : 'White']
+print(f) #[20 , 18 , 15, 10 , 5]
 print(a) #{10 : 'Red' , 20 : 'Green' , 15 : 'Blue' , 18 : 'Yellow' , 5 : 'White'}
 
  '''
