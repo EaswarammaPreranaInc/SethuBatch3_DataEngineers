ad040a32fd9358ef3f1a19fcdc480dec242b48b0
Shirisha <lovelysirii19@gmail.com>
2026-03-25 15:19:14 +0530

    Refactor functions and add prime number generation
    Refactor functions to ensure all are executed and implement prime number generation.

--- submissions/SALENDRI_SHIRISHA_20260324.py
+++ submissions/SALENDRI_SHIRISHA_20260324.py
@@ -1,12 +1,16 @@
 # Modify  following  program  such  that  every  function  should  be  executed
 def  f1():
 	print('No-argument  function')
+f1()
 def  f1(x):
 	print('Single  argument  function  : ' , x)
+f1()
 def  f1(x , y):
 	print('Two  argument  function : ' , x , y)
+f1()
 def  f1(x , y , z):
 	print('Three  argument  function : ' , x , y , z)
+f1()
 
 
 '''
@@ -18,9 +22,15 @@ Hint:  Use  the  prime()  function  defined  in   prog3a.py  but  do  not  rewri
 What  are  the  outputs  if  input  is  10  ?  --->  Prime numbers : [2 , 3 , 5 , 7]
 																		    Number  of   prime  numbers : 4
 '''
+from prog3a import prime
 def  prime_numbers(n):
-	return  list  of  all  the  prime  numbers  between  2  and   n
-Read  'n'  value
-call  prime_numbers()  function  
-print  the  list
-print  number  of  prime  numbers
+	list = []
+	for i in range(2,n+1):
+		if prime(i):
+			list.append(i)
+	return list
+n = int(input('Enter any number: '))
+x =  prime_numbers(n)
+print('Prime Numbers: ' , x)
+print('Number of prime numbers: ' , len(a))
+
