f79d70f5b56cb16fa8c916c34597c280164fb05d
rachamallagayathri12-cpu <rachamallagayathri12@gmail.com>
2026-03-30 19:50:17 +0530

    Add files via upload

---
+++ submissions/Rachamalla_Gayathri_2026_03_30.py
@@ -0,0 +1,27 @@
+ # Tricky  program
+# Find  outputs
+a = 10
+b = 7
+x = lambda  a , b :  a + b ,  a - b , a * b , a / b
+print(type(x))#<class 'tuple'>
+print(x)#(17,3,70,1.4)
+for  y  in   x:
+	print(y)#3 70 1.4
+print(x[0](9 , 2))#11
+print(x[1])#3
+print(x())#error
+#  Tricky  program
+# Find  outputs
+a  =  lambda  :  print('Hyd')  ;  print('Sec');  print('Cyb')
+print(a())#hyd sec cyb
+ # Find  outputs (Home  work)
+a  =  lambda  :   'Hyd' ;  print('Sec') ;  print('Cyb')
+print(a()) #sec cyb
+ # Find  outputs   (Home  work)
+a  =  lambda  :  print('Hyd')  , print('Sec')  , print('Cyb')
+print(type(a)) #<class 'tuple'>
+print(a) ('hyd','sec','cyb')
+for  x  in  a:
+	print(x)
+a() #error
+print(a[0]())#none
