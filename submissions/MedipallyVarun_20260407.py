1c74e1567e7e79ba0734d42fd96f088a7f4f9cfc
Medipally Varun <varungoudmedipally@gmail.com>
2026-04-08 07:32:34 +0530

    Assignment

--- submissions/MedipallyVarun_20260407.py
+++ submissions/MedipallyVarun_20260407.py
@@ -1,210 +1,180 @@
+'''  (Home  work)
+Write  a  program  to  determine  area  and  perimeter  of  triangle  and  represent  triangle  by  an  object
 
+1) What  is  the  area  of  triangle ?  --->  sqrt(s * (s - a) * (s - b) * (s - c))
+
+2) What  is  the  formula  for  's' ?  --->  (a + b + c) / 2
+
+3) What  is  the  perimeter  of  triangle ?  ---> a + b + c
 '''
-Write  a  generator  to  divide  a  string  into  words
-
-Hint1:  Use  generator  function  and  for   loop
-
-Hint2:  Use  split()  method  of  str  class
-'''
-import time
-def   words(s):
-	list = s . split()  #  ['Hyd' , 'is' , 'green' , 'city']
-	for  x  in  list:
-		yield  x
-# End of generator
-s = input('Enter  any   string  :  ')#   M.G.Story
-g = words(s) #  Gen  object
-print('Words  of  the  string')
-for  y  in   g:
-	print(y)  #  Hyd , is , green , city
-	time . sleep(1)
-Modify  the  program  without  using  list
-
-
-
-#  Find  outputs (Home  work)
-def   f1():
-	x = 1
-	while  x <= 100000000000000000000:
-		yield  x
-		x +=  1
-# End of  generator
-g = f1()
-print('Begin')  # Begin
-print(*g)  # memory space error
-print('End')  # end
-
-
+import  math
+class  triangle:
+	def  get(self):
+		self.a = float(input('Enter first side: '))
+		self.b = float(input('Enter second side: '))  
+		self.c = float(input('Enter third side: '))#How  to  read  three  sides  into  object  
+	def  test(self):
+		if (self.a + self.b >= self.c)and (self.b + self.c >= self.a) and (self.c + self.a >= self.c):
+				pass
+		else:
+				print('Not  a  triangle')
+				exit()
+	def  area(self):
+			s = (self.a + self.b +self.c) /2
+			area = math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+			return area
+	def  peri(self):
+			p = self.a + self.b + self.c
+			return  p
+# End of the class
+t = triangle()
+t.get()
+t.test()
+print('Area : ',   t.area())
+print('Perimeter : ',  t.peri())
+	  
 
 
 #  Find  outputs  (Home  work)
-g = (x * x  for  x  in  range(500000000000000000))
-print(*g)
-# runs continuously
-
-
-#  Find  outputs (Home  work)
-def  f1():
-	print('One')
-	yield    1
-	print('Two')
-	yield    2
-	print('Three')
-	yield    3
-	print('End')
-# End  of  generator
-g = f1()
-for   m   in   g:
-	print(m)  #  One
-1
-Two
-2
-Three
-3
-End
-x ,  y ,  z  =  f1()
-print(x)
-print(y) #One
-#Two
-#Three
-print(z)  # 1
-2
-3
-
-
-
-# Identify  error (Home  work)
-def  f1():
-        yield  10
-        yield  20
-        yield  30
-        yield  40
-a , b , c = f1()  
-p , q , r , s , m = f1()  # 5 element are there
-
-
-
-#  Find  outputs (Home  work)
-def   f1():
-	yield    1
-	yield    2
-	yield    3
-# End  of  generator
-g =  f1()
-print(len(g))  
-print(g * 3)  
-print(g[0])  
-print(g[1 : 3])
-print(*g)
-
-
-
-
-# Identify  error  (Home work)
 class   c1:
 	def  m1(self):
-		pass
-class   c2:
-        pass
-class   c3:  # no statements in c3
-
-
-
-# Find  outputs  (Home  work)
-class   c1:
-	pass
+		x = 10
+		self . x = 20
+		print(x) # 10
+		print(self . x) #20
+		x += 5 #15
+		self . x += 7 #27
+	def   m2(self):
+		print(x) # Error
+		print(self . x) # 27
+		self . x += 6 # 33
 # End  of  the  class
 a = c1()
-print(id(a))   # address
-print(type(a))  # <class '__main__.c1'>
-print(a . __dict__)  # {}
-print(a)  # error
-del  a 
-print(a)
+a . m1()
+a . m2()
+print(a . x) # 33
+print(self . x) # Error 
+print(x) # Error
 
 
 
+'''  (Home  work)
+Write  a  program  to  add  two  objects  where  each  object  contains  three  values  and
+store  results  in   third  object
 
+1st  object   --->  x = 10 , y = 20 , z = 30
 
+2nd  object --->  x = 40 , y = 50 , z = 60
 
-#  Find  outputs  (Home  work)
-def   m1():
-		print('Function')
-class   c1:
-	def   m1(self):
-		print('1st  method')
-	def   m1(self):
-		print('2nd  method')
-	def   m1(self):
-		print('3rd  method')
-# End  of  class  c1
-a = c1()  #
-a . m1()  # 3rd function
-m1()  # error method cannot be directly called
+3rd  object  ---> x = 10 + 40 = 50 , y = 20 + 50 = 70 , z = 30 + 60 = 90
+'''
+class Test:
+    def get(self):
+        self.x = int(input('Enter the value of x : '))
+        self.y = int(input('Enter the value of y : '))
+        self.z = int(input('Enter the value of z : '))
+    def add(self, m, n):
+        self.x = m.x + n.x
+        self.y = m.y + n.y
+        self.z = m.z + n.z
+    def disp(self):
+        print(f'x = {self.x}, y = {self.y}, z = {self.z}')
 
-
-#  Find  outputs  (Home  work)
-class   c1:
-	def   m1(self):
-		print('No  argument  method')
-	def   m1(self , x):
-		print('Single  argument  method : ' , x)
-	def   m1(self , x , y):
-		print('Two  argument  method : ' , x , y)
-# End  of  class  c1
-a = c1()   # error
-a . m1(10 , 20)  #  Two argument method : 10 , 20
-a . m1(30)  # error
-a . m1() # error no arguments
-
-
-
-# Find  outputs  (Home  work)
-class   c1:
-	def   m1(self):
-		print('Method  of  first  c1  class')
-class   c1:
-	def   m1(self):
-		print('Method  of  second  c1  class')
-class   c1:
-	def   m1(self):
-		print('Method  of  third  c1  class')
-a = c1()
-a . m1()  3 method of third c1 class
-
-
-# Find  outputs  (Home  work)
-class   c1:
-	def   m1(self):
-		print('Method  of  first  c1  class')
-class   c1:
-	def   m1(self):
-		print('Method  of  second  c1  class')
-class   c1:
-	pass
-a = c1()
-a . m1()   # 
+# End of the class
+a = Test()
+b = Test()
+c = Test()
+print('First Object')
+a.get()
+print('Second Object')
+b.get()
+c.add(a, b)
+print('Addition results')
+c.disp()
 
 
 #  Find  outputs (Home  work)
-class  c1:
-        pass
-# End  of  class
+class  Date:
+	pass
+# End of the class
+a =  Date()
+a . dd = 15
+a . mm = 8
+a . yy = 1947
+print(a) # type and address 
+
+
+
+#  Object  'a'  --->
+#  Find  outputs (Home  work)
+class   c1:
+	def  __str__(self):
+			return  '25'
+class   c2:
+	def  __str__(self):
+			return   35
+class   c3:
+	def  __str__(self):
+			print('Hyd')
+class   c4:
+	def  __str__(self , x):
+			return   F'{x}'
+#end of the class
 a = c1()
-print(a . __dict__)
-a . x = 10
-print(a . __dict__)
-a . y = 20
-print(a . __dict__)
-a . x = 30
-print(a . __dict__)
-a . y = 40
-print(a . __dict__)
-del  a . x
-print(a . __dict__)
-del  a . y
-print(a . __dict__)
-del   a
-print(a . __dict__)
+b = c2()
+c = c3()
+d = c4()
+print(a) # 25
+print(b) # Error
+print(c) # Error
+print(d) # Error 
+print(b . __str__()) # 35 
+print(c . __str__()) # Hyd, None 
+print(d . __str__(50)) # 50 
+
+'''
+Write  a  program  to  determine  total , average  and  grade  of  a  student
+Inputs  are  Roll Number , Stud  Name , Marks  of  3  subjects  and  Gender
+'''
+class Student:
+    def get(self):
+        self.r = int(input('Enter any roll no: '))
+        self.s = input('Enter Student name : ')
+        self.g = input('Enter gender (m/f): ')
+        self.marks = []
+        for i in range(3):
+            self.marks.append(int(input(f'Enter marks for subject {i+1} : ')))
+
+    def compute(self):
+        self.total = float(sum(self.marks))
+        self.average = self.total / 3
+
+        if any(mark < 40 for mark in self.marks):
+            self.grade = 'Fail'
+        elif self.average >= 70:
+            self.grade = 'Distinction'
+        elif self.average >= 60:
+            self.grade = 'First class'
+        elif self.average >= 50:
+            self.grade = 'Second class'
+        else:
+            self.grade = 'Third class'
+
+    def disp(self):
+        print('Roll Number :', self.r)
+        print('Student Name :', self.s)
+        print('Gender :', self.g)
+        print('Total Marks :', self.total)
+        print('Average :', round(self.average, 2))
+        print('Grade :', self.grade)
+
+    def __str__(self):
+        return f'{self.r}, {self.s}, {self.g}, {self.total}, {round(self.average, 2)}, {self.grade}'
 
 
+# Object creation
+c = Student()
+c.get()
+c.compute()
+c.disp()
+print(c.__str__())   # better way instead of c.__str__()
