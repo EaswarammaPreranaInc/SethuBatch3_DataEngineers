889db66409f5ed8aea04cc5cbeb54adfd2669c16
Pradeep-Dasi <pradeepdasi006@gmail.com>
2026-04-08 23:44:45 +0530

    Add files via upload

--- submissions/Pradeep_Dasi_20260408.py
+++ submissions/Pradeep_Dasi_20260408.py
@@ -1,70 +1,122 @@
-import time
-def words(s):
-    word = ''
-    for x in s:
-        if x != ' ':
-            word+=x
-        else:
-            yield word
-            word =''
-    if word:
-        yield word
-s = input("enter the string: ")
-g = words(s)
-for y in g:
-    print(y)
-    time.sleep(1)
-
-def   f1():
-    x = 1
-    while  x <= 100000000000000000000:
-        yield  x
-        x +=  1
-# End of  generator
-g = f1()
-print('Begin')
-print(*g)
-print('End')
-
-
 #  Find  outputs  (Home  work)
-g = (x * x  for  x  in  range(500000000000000000))
-print(*g)
-
-#  Find  outputs (Home  work)
-def  f1():
-    print('One')
-    yield    1
-    print('Two')
-    yield    2
-    print('Three')
-    yield    3
-    print('End')
-g = f1()
-for   m   in   g:
-    print(m)
-x ,  y ,  z  =  f1()
+class   c1:
+	def  m1(self):
+		x = 10
+		self . x = 20
+		print(x)
+		print(self . x)
+		x += 5
+		self . x += 7
+	def   m2(self):
+		print(x)
+		print(self . x)
+		self . x += 6
+# End  of  the  class
+a = c1()
+a . m1()
+a . m2()
+print(a . x)
+print(self . x)
 print(x)
-print(y)
-print(z)
+'''  (Home  work)
+Write  a  program  to  add  two  objects  where  each  object  contains  three  values  and
+store  results  in   third  object
 
-# Identify  error (Home  work)
-def  f1():
-        yield  10
-        yield  20
-        yield  30
-        yield  40
-a , b , c = f1()  
-p , q , r , s , m = f1()
+1st  object   --->  x = 10 , y = 20 , z = 30
 
+2nd  object --->  x = 40 , y = 50 , z = 60
+
+3rd  object  ---> x = 10 + 40 = 50 , y = 20 + 50 = 70 , z = 30 + 60 = 90
+'''
+class  Test:
+	def   get(self):
+		 How  to  read  inputs  into  variables  x , y  and  z  
+	def   add(self , m , n):
+		 How  to  add  objects  m  and  n  and  store  results  in  object 
+	def  disp(self):
+		 How  to  print  object  
+# End  of  the  class
+How  to  create  three  Test  class  objects  a , b  and  c
+print('First  Object')
+How  to  read  inputs  into  object  'a'
+print('Second  Object')
+How  to  read  inputs  into  object  'b'
+How  to  add  objects  a  and  b  and  store  results in  object  'c'
+print('Addition  results')
+How  to  print  object  'c'
 #  Find  outputs (Home  work)
-def   f1():
-    yield    1
-    yield    2
-    yield    3
-g =  f1()
-print(len(g))
-print(g * 3)
-print(g[0])
-print(g[1 : 3])
-print(*g)
+class  Date:
+	pass
+# End of the class
+a =  Date()
+a . dd = 15
+a . mm = 8
+a . yy = 1947
+print(a)
+
+
+
+#  Object  'a'  --->
+#  Find  outputs (Home  work)
+class   c1:
+	def  _str_(self):
+			return  '25'
+class   c2:
+	def  _str_(self):
+			return   35
+class   c3:
+	def  _str_(self):
+			print('Hyd')
+class   c4:
+	def  _str_(self , x):
+			return   F'{x}'
+#end of the class
+a = c1()
+b = c2()
+c = c3()
+d = c4()
+print(a)
+print(b)
+print(c)
+print(d)
+print(b . _str_())
+print(c . _str_())
+print(d . _str_(50))
+'''
+Write  a  program  to  determine  total , average  and  grade  of  a  student
+Inputs  are  Roll Number , Stud  Name , Marks  of  3  subjects  and  Gender
+'''
+class   Student:
+	def   get(self):
+		How  to  read  roll  number  
+		How  to  read  student  name  
+		How  to  read  gender  
+		How  to  read  marks  of  3  subjects
+	def   compute(self):
+		How  to  calculate  total  marks
+		How  to  calculate  average  marks
+		if  At  least  one  subject  is  below  40:
+				How  to  initilaize  grade  to  'Fail'
+		elif  average  is  above  >= 70%:
+				How  to  initilaize  grade  to  'Distinction'
+		elif  average  is  above  >= 60%:
+				How  to  initilaize  grade  to  'First  class'
+		elif  average  is  above  >= 50%:
+				How  to  initilaize  grade  to  'Second  class'
+		else:
+				How  to  initilaize  grade  to  'Third  class'
+	def  disp(self):
+		print('Roll  Number  :  ' ,   ???)
+		print('Student  Name  :  ' , ???)
+		print('Gender  :  ' ,  ???)
+		print('Total  Marks  :  ' , ???)
+		print('Average  :  ' , ???)
+		print('Grade  :  ' , ???)
+	def   _str_(self):
+		return  All  the   values  of  object  in  the  form  of  string
+#End  of  the  class
+How  to  create  Student  class  object
+How  to  read  inputs  into  object
+How  to  store  results  in  object
+How  to  print  object  with  disp()  method
+How  to  print  object  with  _str_()  method
