fca45660dfb231056876bdd6e71c515445d52adf
Bhavishya94 <bhavishya.arukonda904@gmail.com>
2026-04-09 06:21:49 +0530

    Add files via upload

--- submissions/Bhavishya_Arukonda_20260408.py
+++ submissions/Bhavishya_Arukonda_20260408.py
@@ -1,206 +1,284 @@
-'''  (Home  work)
-Write  a  program  to  determine  area  and  perimeter  of  triangle  and  represent  triangle  by  an  object
+'''
+Repeat  prog5a  such  that  methods  are  called  in  a  different  way
 
-1) What  is  the  area  of  triangle ?  --->  sqrt(s * (s - a) * (s - b) * (s - c))
+1) What  are  the  two  ways  to  call  a  method ?  ---> object . method()  and  classname . method(object)
 
-2) What  is  the  formula  for  's' ?  --->  (a + b + c) / 2
+2) Reuse  triangle  class  defined  in  prog5a  but  do  not  define  triangle  class  again
+'''
+How  to  create  triangle  object
+How  to  call  get()  method  in  another  way
+How  to  call  test()  method  in  another  way
+print('Area : ',  How  to  call  area()  method  in  another  way)
+print('Perimeter: ',  How  to  call  peri()  method  in  another  way)
 
-3) What  is  the  perimeter  of  triangle ?  ---> a + b + c
+
+# Create object
+t = triangle()
+
+# Call methods using classname.method(object)
+triangle.get(t)
+triangle.test(t)
+
+print("Area :", triangle.area(t))
+print("Perimeter :", triangle.peri(t))
+
+
+
+# What  are  the  outputs  if  inputs  are  25 , Rama  Rao ,  male , 52 , 48 , 55   (Home  work)
+from  prog9a  import  student
+s = student()
+print(s . __dict__)
+s . get()
+print(s . __dict__)
+s . compute()
+print(s . __dict__)
+
+
+{}
+{'rno': 25, 'sname': 'Rama Rao', 'gender': 'male', 'm': [52, 48, 55]}
+{'rno': 25, 'sname': 'Rama Rao', 'gender': 'male', 'm': [52, 48, 55], 'tot': 155, 'avg': 51.666666666666664, 'grade': 'Second  class'}
+
+
+
+
+'''
+Repeat  student  program  for  'n'  students
+
+1) import  student  class  defined in  prog9a  but  do  not  rewrite
+
+2) Use  list  of  objects
+'''
+from  prog9a  import  student
+How  to  read  number  of  students  into   variable  'n'
+How  to  store  'n'  student  class  objects   in  list  'a'
+How  to   read  each  student  data  into   the  object  held  by  the  list
+How  to   store  results  in   each  object  held  by  the  list
+How  to   print  each  object  held  by  the  list
+
+from prog9a import student
+
+# Read number of students
+n = int(input("Enter number of students: "))
+
+# Create list for student objects
+a = []
+
+# Read data and compute for each student
+for i in range(n):
+    print(f"\nEnter details of student {i+1}")
+    s = student()
+    s.get()
+    s.compute()
+    a.append(s)
+
+# Print all student objects
+print("\nStudent Details")
+for x in a:
+    print(x)
+
+
+'''
+Write  a  program  to  add , subtract , multiply  and  divide  two  rational  numbers
+
+1) 1st  rational  number  --->  2 / 3
+    2nd  rational  number  --->   5 / 9
+    What  is  the  sum  ?  ---> 2 / 3 + 5 / 9 =  (18 + 15) / 27 = 33 / 27 =  11 / 9
+    What  is  the  difference  ?  --->  2 / 3 - 5 / 9 =  (18 - 15) / 27 = 3 / 27 = 1 / 9
+    What  is  the  product  ?  --->  2 / 3 * 5 / 9 =  10 / 27 = 10 / 27
+    What  is   the  division  ?  --->  2 / 3 /  5 / 9 = 2 / 3 * 9 / 5 =  18 / 15 =  6 / 5  --->  Succesful  division
+
+2) 1st  rational  number  --->  2 / 3
+    2nd  rational  number  --->   0 / 9
+    What  is  the  sum  ?  --->  2 / 3 + 0 / 9 =  (18 + 0) / 27 =  18 / 27 =  2 / 3
+    What  is  the  difference  ?  ---> 2 / 3 - 0 / 9 =  (18 - 0) / 27 =  18 / 27 =  2 / 3
+    What  is  the  product  ?  --->  2 / 3 * 0 / 9 =  0 / 27  =  0 / 27  --->  Simplification  is  not  required  becoz  numerator  is  0
+    What  is   the  division  ?  --->  2 / 3 /  0 / 9 =  2 / 3 * 9 / 0 =  18 / 0  ---> Division  is  not   permitted
+
+3) When  is  simplification  needed ?  --->  When  numerator  is  non-zero
 '''
 import  math
-class  triangle:
+class  Rat:
 	def  get(self):
-		How  to  read  three  sides  into  object  
+		How  to  read  numerator  
+		How  to  read  denominator 
+		How  to  call  test()  method
 	def  test(self):
-		if  sum  of  every  2  sides  >=  3rd  side:
-				Do  nothing
-		 else:
-				print('Not  a  triangle')
-				How  to  stop  execution
-	def  area(self):
-			return   area  of  triangle
-	def  peri(self):
-			return  perimeter  of  triangle
-# End of the class
-How  to  create  triangle  class  object
-How  to  read  inputs  into  object
-How  to  test  whether  inputs  are  valid
-print('Area : ',   ???)
-print('Perimeter : ',  ???)
+		Ask  user  to  reenter  denom  when  denom  is  zero
+	def    __str__(self):
+			 return  values  of  object  in  the  form  of  rational  number  such   as  '2 / 3'
+	def   add(self , a , b):
+		How  to  add  objects  'a'  and  'b' and  store  results  in  object  
+		How  to  simplify  object  
+	'''
+	c . add(a , b)
+	object  a  --->  2 / 3
+	object  b  --->  5 / 9
+	object  c  --->  2 / 3 + 5 / 9 = (2 * 9 + 5 * 3) / (5 * 9) = 33 / 27 = 11 / 9
+	'''
+	def   sub(self , a , b):
+		How  to  subtract  objects  'a'  and  'b' and  store  results  in  object 
+		How  to  simplify  object  
+	'''
+	d . sub(a , b)
+	object  a  --->  2 / 3
+	object  b  --->  5 / 9
+	object  d  --->  2 / 3 - 5 / 9 = (2 * 9 - 5 * 3) / (5 * 9) = 3 / 27 = 1 / 9
+	'''
+	def   mul(self , a , b):
+		How  to  multiply  objects  'a'  and  'b' and  store  results  in  object  
+		How  to  simplify  object 
+	'''
+	e . mul(a , b)
+	object  a  --->  2 / 3
+	object  b  --->  5 / 9
+	object  e  --->  2 / 3 * 5 / 9 = (2 * 5) / (3 * 9) = 10 / 27
+	'''
+	def    div(self , a , b):
+		How  to  divide  objects  'a'  and  'b' and  store  results  in  object  
+		How  to  simplify  object 
+	'''
+	f . div(a , b)
+	object  a  --->  2 / 3
+	object  b  --->  5 / 9
+	object  f  --->  2 / 3 / 5 / 9 = 2 / 3 * 9 / 5 = (2 * 9) / (3 * 5) = 18 / 15 = 6 / 5
+	'''
+	def   simplify(self):
+			How  to  find  gcd  of  numerator  and   denominator
+			How  to  simplify  rational  number  in  object  self  i.e.  12 / 15  should  be  simplified  to  4 / 5
+	'''
+	c . simplify()
+	1)  12 / 15  --->  4 / 5
+	2) 10 / 27   --->  10 / 27
+	3) 0 / 27  --->   0 / 27
+	'''
+# End  of the class
+How  to  create  6  objects  a , b , c , d , e , f
+How  to  read  rational  number  into  object  'a'
+How to  read  rational  number  into  object  'b'
+How  to  add  rational  numbers  in  objects  a  and  b  and  store  results  in  object  'c'
+How  to  subtract  rational  numbers  in  objects  a  and  b  and  store  results  in  object  'd'
+How  to multiply  rational  numbers  in  objects  a  and  b  and  store  results  in  object  'e'
+How  to  divide  rational  numbers  in  objects  a  and  b  and  store  results  in  object  'f'
+How  to  print  object   'c'
+How  to  print  object   'd'
+How  to  print  object   'e'
+if  ???
+	How  to  print  object  'f
+else:
+	print('Division  is  not  permitted')
 
 
-import math
+'''
+Object  'a'  --->  
 
-class triangle:
-    def get(self):
-        self.a = float(input("Enter first side: "))
-        self.b = float(input("Enter second side: "))
-        self.c = float(input("Enter third side: "))
+Object  'b'  --->  
 
-    def test(self):
-        if self.a + self.b > self.c and self.a + self.c > self.b and self.b + self.c > self.a:
-            return
-        else:
-            print("Not a triangle")
-            exit()
+Object  'c'  --->  
 
-    def area(self):
-        s = (self.a + self.b + self.c) / 2
-        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+Object  'd'  --->  
 
-    def peri(self):
-        return self.a + self.b + self.c
+Object  'e'  --->  
+
+Object  'f'  --->  
+'''
 
 
-t = triangle()
-t.get()
-t.test()
 
-print("Area :", t.area())
-print("Perimeter :", t.peri())
-
+#  dir()  function  demo  program  (Home  work)
+from  prog10a   import  Rat
+a = Rat()
+a . nr = 22
+a . dr = 7
+print(dir(Rat))
+print()
+print()
+print(dir(a))
 
 
 #  Find  outputs  (Home  work)
-class   c1:
-	def  m1(self):
-		x = 10
-		self . x = 20
-		print(x)  # 10
-		print(self . x)   # 20
-		x += 5 # x = 15
-		self . x += 7   # 27
-	def   m2(self):
-		print(x)
-		print(self . x)
-		self . x += 6
+class      Rat:
+	def    m1():
+		pass
 # End  of  the  class
-a = c1()
-a . m1()  # 
-a . m2()
-print(a . x)  # 27
-print(self . x)  # error
-print(x)  # error
+a = Rat()
+a . nr = 22
+print(hasattr(a , 'nr'))
+print(hasattr(a , 'dr'))
+print(hasattr(a , 'm1'))
+print(hasattr(a , 'm2'))
+print(hasattr(Rat , 'm1'))
+print(hasattr(Rat , 'm2'))
+print(hasattr(Rat , 'nr'))
 
 
 
-'''  (Home  work)
-Write  a  program  to  add  two  objects  where  each  object  contains  three  values  and
-store  results  in   third  object
-
-1st  object   --->  x = 10 , y = 20 , z = 30
-
-2nd  object --->  x = 40 , y = 50 , z = 60
-
-3rd  object  ---> x = 10 + 40 = 50 , y = 20 + 50 = 70 , z = 30 + 60 = 90
-'''
-class  Test:
-	def   get(self):
-		 How  to  read  inputs  into  variables  x , y  and  z  
-	def   add(self , m , n):
-		 How  to  add  objects  m  and  n  and  store  results  in  object 
-	def  disp(self):
-		 How  to  print  object  
-# End  of  the  class
-How  to  create  three  Test  class  objects  a , b  and  c
-print('First  Object')
-How  to  read  inputs  into  object  'a'
-print('Second  Object')
-How  to  read  inputs  into  object  'b'
-How  to  add  objects  a  and  b  and  store  results in  object  'c'
-print('Addition  results')
-How  to  print  object  'c'
-
-
-#  Find  outputs (Home  work)
-class  Date:
-	pass
-# End of the class
-a =  Date()
-a . dd = 15
-a . mm = 8
-a . yy = 1947
-print(a)  # (15 8 1947)
+# Object  'a'  --->
 
 
 
-#  Object  'a'  --->
-
-
-
-#  Find  outputs (Home  work)
-class   c1:
-	def  __str__(self):
-			return  '25'
-class   c2:
-	def  __str__(self):
-			return   35
-class   c3:
-	def  __str__(self):
-			print('Hyd')
-class   c4:
-	def  __str__(self , x):
-			return   F'{x}'
+# Find  outputs  (Home  work)
+class  Cat:
+	def  talk(self):
+		print('Meow Meow Meow ....')
+class  Dog:
+	def  bark(self):
+		print('Bhow Bhow Bhow ....')
+class  Goat:
+	def  talk(self):
+		print('Mehar  Mehar  Mehar  ....')
 #end of the class
-a = c1()  
-b = c2() 
-c = c3() 
-d = c4()  
-print(a)  # 25
-print(b)   # error
-print(c)   # error
-print(d)   # error
-print(b . __str__())   # error
-print(c . __str__())   # hyd
-print(d . __str__(50))  # 50
+a = [Cat() , Dog() , Goat()]
+for  x  in   a:
+	if   hasattr(x , 'talk'):
+		x . talk()
+	else:
+		x . bark()
 
+
+Meow Meow Meow ....
+Bhow Bhow Bhow ....
+Mehar  Mehar  Mehar  ....
+
+
+#  Find  outputs  (Home  work)
+class    c1:
+        pass
+# End of the class
+a = c1()
+a . x = 10
+varname = input('Enter  variable  name  to  be  added  to  object  :  ')   #  Assume  that  input  is  'y'
+value = eval(input('Enter  value  of  the  variable  :  '))   #  Assume  that  input  is   20
+setattr(a , varname , value)
+print(a . __dict__)
+print(a . x) # 10
+while  True:
+	try:
+		varname = input('Enter  variable  name  whose  value  is  to  be  retrieved  :  ')
+									#  Assume  that  input  is  x  in  1st   iteration  ,  y  in   2nd   iteration  and  z  in  3rd  iteration
+		print(getattr(a , varname))
+	except:
+		print(F'Invalid  variable   name   :  {varname}')
+		break
+
+
+{'x': 10, 'y': 20}
+10
+10
+20
+Invalid  variable   name   :  z
 
 
 
 '''
-Write  a  program  to  determine  total , average  and  grade  of  a  student
-Inputs  are  Roll Number , Stud  Name , Marks  of  3  subjects  and  Gender
+(Home  work)
+Write  a  program  to  convert  a  dictionary  {'Empno' : 25 , 'Ename' : 'Rama  Rao' , 'Sal' : 10000.0}  to  Emp  class  object
+i.e.  object  should  contain  empno = 25 , ename = 'Rama  Rao' , Sal = 10000.0
+
+Hint:  Use  setattr()  and  getattr()  functions
 '''
-class   Student:
-	def   get(self):
-		How  to  read  roll  number  
-		How  to  read  student  name  
-		How  to  read  gender  
-		How  to  read  marks  of  3  subjects
-	def   compute(self):
-		How  to  calculate  total  marks
-		How  to  calculate  average  marks
-		if  At  least  one  subject  is  below  40:
-				How  to  initilaize  grade  to  'Fail'
-		elif  average  is  above  >= 70%:
-				How  to  initilaize  grade  to  'Distinction'
-		elif  average  is  above  >= 60%:
-				How  to  initilaize  grade  to  'First  class'
-		elif  average  is  above  >= 50%:
-				How  to  initilaize  grade  to  'Second  class'
-		else:
-				How  to  initilaize  grade  to  'Third  class'
-	def  disp(self):
-		print('Roll  Number  :  ' ,   ???)
-		print('Student  Name  :  ' , ???)
-		print('Gender  :  ' ,  ???)
-		print('Total  Marks  :  ' , ???)
-		print('Average  :  ' , ???)
-		print('Grade  :  ' , ???)
-	def   __str__(self):
-		return  All  the   values  of  object  in  the  form  of  string
+class  Emp:
+        pass
 #End  of  the  class
-How  to  create  Student  class  object
-How  to  read  inputs  into  object
-How  to  store  results  in  object
-How  to  print  object  with  disp()  method
-How  to  print  object  with  __str__()  method
-
-
-
-#  Object  's'  --->
-
-
-
-
-
+dict = {'Empno' : 25 , 'Ename' : 'Rama  Rao' , 'Sal' : 10000.0}
+How  to  convert  dictionary  to  object  'e'  with  for  loop
+How  to  print  object  'e'  with  for  loop
