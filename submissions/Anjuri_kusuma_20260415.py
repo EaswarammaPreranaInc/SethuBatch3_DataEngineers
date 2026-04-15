eea0f9c8d02ba71de3bbc3771bf03aa35421a2b0
ANJURI KUSUMA <124018751+Anjurikusuma@users.noreply.github.com>
2026-04-15 23:13:45 +0530

    Add files via upload

--- submissions/Anjuri_kusuma_20260415.py
+++ submissions/Anjuri_kusuma_20260415.py
@@ -1,163 +1,182 @@
+ ''
+Write  a  program  to  overload  + ,   -  ,  *   and   /   operators  on  complex  class  objects  without  using  pre-defined
+complex  object
+
+1) First  rational  number  --->  3 + 4i
+   Second  rational  number ---> 5 + 6i
+   What  is  the  sum  ?  --->  8 + 10i
+   What  is  the  difference  ?  --->  -2 - 2i
+   What  is  the  product  ?  ---> (3 + 4i) * (5 + 6i) = 15 + 18i + 20i - 24 =  -9 + 38i
+   What  is   the  division  ?  --->  (3 + 4i) / (5 + 6i) =  (3 + 4i) * (5 - 6i) / (5 + 6i) * (5 - 6i) =  (15 - 18i + 20i + 24) / (25 + 36) = 
+																																							39 / 61 + 2i / 61
 '''
-Repeat  prog10a  with  3  objects          #from prog10a import rat
-                                           #a=rat()
-Eg:  c = a + b                             #b=rat()
-	 print  c                          #c=a+b
-	 c = a - b
-	 print  c
-	 c = a * b
-	 print  c
-	 c = a / b
-	 print  c
+import  math
+class  complex:
+	def  get(self):
+		self.real=int(input("Enter real number : "))  #How  to  read  real  and  imag
+                self.imag=int(input("Enter imag number : "))
+	def    _str_(self):
+		 if self.imag>=0:   #How  to  return  real  and  imag  in  the  form  of  3 + 4i  (or)  3 - 4i
+                    f'{self.real}+{self.imag}i 
+	def  _add_(a ,  b):
+		How  to  add  objects  a  and  b
+	def  _sub_(a ,  b):
+		How  to  subtract  objects  a  and  b
+	def  _mul_(a ,  b):
+		How  to  multiply  objects  a  and   b
+	def  _truediv_(a ,  b):
+		How  to  divide  objects   a  and  b
+# End  of  the  class
+How  to  create  two  complex  class  objects
+How  to  read   inputs  into  1st  object
+How  to  read   inputs  into  2nd  object
+print('Sum :  ' , ???)
+print('Difference :  ' , ???)
+print('Product :  ' ,  ??)
+print('Division  : ' , ???)
+-----------------------------------------------------------------------------------
+ '''
+Overload   > ,  < ,  == ,  >=  , <=  , !=  on   Rational   class  objects
 
-Hint:  Import   Rat  class  defined  in  prog10a  but  do  not  define  Rat  class   again
+1) Let  object  'a'   contain   2 / 3  and   object  'b'  contain  5 / 9
+    What  is  the  result  of  a > b ?  --->  True  due  to 18 > 15
+    What  is  the  result  of  a < b ?  ---> False  due  to  18  is  not  <  15
+    What  is  the  result  of  a == b ?  ---> False  due  to  18  is  not  =  15
+    What  is  the  result  of  a >= b ?  ---> True  due  to 18 >= 15
+    What  is  the  result  of  a <= b ?  ---> False  due  to  18  is  not  <=  15
+    What  is  the  result  of  a != b ?  --->  True  due  to 18 != 15
+
+2) Imp  point  is  cross  product
+
+3) What  is  the  method  call  to  _gt()  method ?  ---> a > b   (or)  a . __gt_(b)
+     What  is  the  method  call  to  _lt()  method ?  ---> a < b  (or)  a . __lt_(b)
+     What  is  the  method  call  to  _eq()  method ?  --->  a == b   (or)  a . __eq_(b)
+     What  is  the  method  call  to  _ge()  method ?  --->  a >= b   (or) a . __ge_(b)
+     What  is  the  method  call  to  _le()  method ?  ---> a <= b  (or)  a . __le_(b)
+     What  is  the  method  call  to  _ne()  method ?  --->  a != b  (or)  a . __ne_(b)
 '''
-------------------------------------------------------------------------------------------
-'''
-Repeat  prog10a  with  list  of  6  objects
+import  math
+class  Rat:
+	def  get(self):
+			 How  to  read  numerator  and  denominator  into  object
+	def _gt_(self,b):
+			return  true  when  rational  number  in  object self  >  that  of  'b'  and  false  otherwise
+	def _lt_(self,b):
+			return  true  when  rational  number  in  object  self  <  that  of  'b'  and  false  otherwise
+	def _eq_(self,b):
+			return  true  when  rational  numbers  in  objects  self   and  'b'  are  same  and  false  otherwise
+	def _ge_(self,b):
+			return  true  when  rational  number  in  object self  >=  that  of  'b'  and  false  otherwise
+	def _le_(self,b):
+			return  true  when  rational  number  in  object self  <=  that  of  'b'  and  false  otherwise
+	def _ne_(self,b):
+			return  true  when  rational  numbers  in  objects  self   and  'b'  are  different  and  false  otherwise
+#  End  of   the  class
+How  to  create  two  Rat   class  objects  'a'  and  'b'
+How  to  read  1st  rational   number  into  object  'a'
+How  to  read  2nd  rational   number  into  object  'b'
+if  1st  rational  is  >  2nd  rational  number
+	print('>')
+if  1st  rational  is  <  2nd  rational  number
+	print('<')
+if  rational  numbers  are  same
+	print('==')
+if  1st  rational  is  >=  2nd  rational  number
+	print('>=')
+if  1st  rational  is  <=  2nd  rational  number
+	print('<=')
+if  rational  numbers  are  different
+	print('!=')
+---------------------------------------------------------------------------------------
+ # Find  outputs  (Home  work)
+class   outer:
+	def  _init_(self):
+		print('Outer  class  constructor')
+	def  m1(self):
+		print('Outer  class  method')
+	class   inner:
+		def _init_(self):
+			print('Inner  class  constructor')
+		def m1(self):
+			print('Inner  class  method')
+#end of the class
+a=outer()   a.m1() #How  to  call  m1()  method  of  outer  class
+b=a.inner()  b.m1() #How  to  call  m1()  method  of  inner  class
+b=outer.inner()   #How  to  call  m1()  method  of  inner  class  in  another  way
+b.m1() #How  to  call  m1()  method  of  inner  class  in  one  more  way
+i = inner()  #error
+----------------------------------------------------------------------------------------
+ # Find  outputs  (Home  work)
+class   emp:
+	def _init_(self):
+		self.empo=25 #How  to  initialize  empno , ename , sal  of  object  self  to  25 ,  'Rama  Rao' , 10000.0
+                self.ename='Rama Rao'
+                self.sal=10000.0
+		self.d=self.date() #How  to  create  date  class  object
+	def   disp(self):
+		print("Empno : ",self.empno) #How  to  print  empno , ename , sal  of  object  self
+                print("Ename : ",self.ename)
+                print("Salary : ",self.sal)
+		self.d.disp() #How  to  call  disp()  method  of  date  class
+	class   date:
+		def    _init_(self):
+			self.dd=15 #How  to  initialize  dd , mm , yy  of  object  self  to  15 , 8  , 1947
+                        self.mm=8
+                        self.yy=1947
+		def disp(self):
+			print("Date :",self.dd,"/",self.mm,"/",self.yy) #How  to  print  dd , mm , yy  of  object  self
+# End  of  the  class
+e=emp() #How  to  call  disp()  method  of  emp  class
+e.disp()
+# Object  'e'  --->
+--------------------------------------------------------------------------------------------
+ # Find outputs (Home  work)
+class  outer:
+	def  _init_(self):
+		self.x=25 #How  to  initialize  variable  'x'  of  object  self  to  25
+		a=outer() b=a.inner1() #How  to  create  inner1  class  object
+		c=a.inner2() #How  to  create  inner2  class  object
+	def  disp(self):
+		print(self . x)
+	class   inner1:
+		def  disp(self):
+			print('1st  inner  class  method')
+	class  inner2:
+		def  disp(self):
+			print('2nd  inner  class  method')
+#end of the class
+a.disp() #How  to  call   disp()  method  of outer  class
+b.disp() #How  to  call   disp()  method  of inner1  class
+c.disp() #How  to  call   disp()  method  of inner2  class
 
-Hint:  import  Rat  class  defined  in  prog10a  but  do  not  rewrite  the  class  again
 
-What  are  the  object  names  ?  --->  a[0] , a[1] , a[2] , .....a[5]
-'''
--------------------------------------------------------------------------------------------
-# mod1.py  (Home  work)
-print('Hyd')        #Hyd
-print('Sec')        #Sec
-print('Cyb')        #Cyb
-#print('India')
-#print('USA')
--------------------------------------------------------------------------------------------
-# Find  outputs  (Home  work)
-import  mod1
-import  mod1
-import  mod1  #this is recognized
------------------------------------------------------------------------------------------------
-# reload()  function  demo  program   (Home  work)
-import    importlib
-import  mod1
-print()
-importlib . reload(mod1)
-print()
-importlib . reload(mod1)
-importlib . reload('mod1')  #error
-reload(mod1)  #error
--------------------------------------------------------------------------------------------------
-#  Find  outputs  (Home  work)
-import  sys , time , math
-print(dir(sys))  #['sys','time','math']
-print()
-print()
-print(dir(time))
-print()
-print(dir(math))
------------------------------------------------------------------------------------------------
-#  Find  outputs  (Home  work)
-import  cal
-print(dir(cal))  #['cal']
------------------------------------------------------------------------------------------------
-#  Find  outputs  (Home  work)
-x = 25
-def  disp():
-	print('Hello')
-class  c1:
-        def  m1(self):
-                pass
-print(dir())    #['x','disp()','c1','m1']
-print(type(dir()))  #<class 'list'>
-print(type(dir))  
--------------------------------------------------------------------------------------------------
-'''
-Write  a  program  to  print  all  the  members  of  cal  module  without  environment  variables
-
-1) What  is  the  result  of  '__name__' . startswith('__')  ?  --->  True
-
-2) What  is  the  result  of  '__spec__' . endswith('__')  ?  --->  True
-
-3) What  is  the  result  of  'spec__' . startswith('__')  ?  --->  False
-
-4) a = []
-    Append  all  the  elements  of  list  returned  by  dir()  function  to  list  'a'  except  environment  variables
-'''
-import cal
-
-a = []
-
-for name in dir(cal):
-    if not (name.startswith('__') and name.endswith('__')):
-        a.append(name)
-
-print(a)
------------------------------------------------------------------------------------------------------
-# cal.py
-def  add(a , b):
-	return  a + b
-def  sub(a , b):
-	return  a - b
-def  mul(a , b):
-	return  a * b
-def  div(a , b):
-	return  a / b
-class    c1:
-	def    m1(self):
-		pass
-#End  of  the  class
-x = 100
-y = 200
-if  __name__   ==  '__main__':
-	print('Hyd')
-	print('Sec')
-	print('Cyb')
-def add(a , b): return a + b
-def sub(a , b): return a - b
-def mul(a , b): return a * b
-def div(a , b): return a / b
-
-class c1:
-    def m1(self):
-        pass
-
-x = 100
-y = 200
-
-if __name__ == '__main__':
-    print('Hyd')
-    print('Sec')
-    print('Cyb')
--------------------------------------------------------------------------------------------------------
-#  Find  outputs
-print(dir())
-print()
-import  cal
-print()
-print(dir())
-------------------------------------------------------------------------------------------------------
-#  Find  outputs
-print(dir())
-print()
-from  cal  import  *
-print()
-print(dir())
-----------------------------------------------------------------------------------------------------
-#  Find  outputs
-print(dir())
-print()
-from  cal  import  add , mul , x
-print()
-print(dir())
-----------------------------------------------------------------------------------------------------
-# sys . path  demo   program
-import  sys
-print('Original  sys.path')
-for  x  in   sys . path:
-	print(x)
-print(len(sys . path))
-import  cal
---------------------------------------------------------------------------------------------------
-# Save  sample.py  module  in  c:\\sairam  folder  before  the  program  is  executed  (Home  work)
-How  to  print  number  of  directories  (or)  folders  in  sys.path
-How  to  append  c:\sairam  folder  to  sys.path
-How  to  print  number  of  directories  (or)  folders  in  sys.path
-How  to  print  object  'x'  of  sample   module  which  is  in  c:\sairam  folder
-How  to  call   function  f1()  of  sample  module  which  is  in  c:\sairam  folder
-How  to  call   method  m1()  of  class  c1  of  sample  module  which  is  in  c:\sairam  folder
+# Object  'o'  --->
+-----------------------------------------------------------------------------------------
+ # Find  outputs  (Home  work)
+class   c1:
+	def  _init_(self):
+		print('outer  class  c1  constructor')
+	class   c2:
+		def _init_(self):
+			print('inner  class  c2  constructor')
+#end of the class
+class  c2:
+	def _init_(self):
+		print('outer  class  c2  constructor')
+#end of the class
+a=c1() #How  to  create  c1  class  object
+b=a.c2() #How  to  create  inner  c2  class  object
+c=c2() #How  to  create  outer  c2  class  object
+-------------------------------------------------------------------------------------
+ # Find  outputs  (Home  work)
+class   c2:
+	def  _init_(self):
+		print('outer  class  constructor')
+	class   c2:
+		def _init_(self):
+			print('inner  class  constructor')
+#end of the class
+a=c2() #How  to  create  outer  c2  class  object
+b=a.c2() #How  to  create  inner  c2  class  object
+b=c2.c2() #How  to  create  inner  c2  class  object  in  another  way
+c2.c2() #How  to  create  inner  c2  class  object  in  one  more  way
