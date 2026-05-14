8747566be4add4ec87982b50f7f01c410ee3ad89
Movva Laxmi Shivani <113294304+Movvalaxmishivani@users.noreply.github.com>
2026-05-14 22:31:38 +0530

    Movva_Laxmishivani_20260512.py

--- submissions/Movva_Laxmishivani_20260512.py
+++ submissions/Movva_Laxmishivani_20260512.py
@@ -3,72 +3,76 @@ Write  a  program  to  evaluate  postfix  expression
 Input :  3 4 5 * + 6 2 / -
 Output :  20
 '''
-def eval(postfix):
-    lst = []
-    for char in postfix:
-        if char.isdigit():
-            lst.append(int(char))
-        elif char != ' ':
-            b = lst.pop()
-            a = lst.pop()
-            if char == '+':
-                result = a + b
-            elif char == '-':
-                result = a - b
-            elif char == '*':
-                result = a * b
-            elif char == '/':
-                result = a // b
-            lst.append(result)
-    return lst.pop()
-postfix = input('Enter postfix expression : ')
-print('Result :', eval(postfix))
+from  prog7b  import  *  
+def  eval(postfix):
+	s = stack() 
+	for  ch  in  postfix:  
+		if  ch . isdigit(): 
+				s . push(int(ch)) 
+		else:  
+				y = s . pop()  
+				x = s . pop()  
+				match  ch:  
+					case   '+':
+								s . push(x + y)  
+					case   '-':
+								s . push(x - y)  
+					case   '*':
+								s . push(x * y)  
+					case   '/':
+								s . push(x // y)   
+					case   '^':
+								s . push(x ** y)  
+	return  s . pop()  
+infix =  input('Enter  infix  expression  :  ')  
+postfix = convert(infix)  
+print('Result : ' ,  eval(postfix))
 
 '''
 Write  a  program  to  convert  infix  to  prefix
 Hint:  Modify  following  program  to  convert  infix  to  prefix
 '''
-from prog1b import stack
-def icp(operator):
-    if operator == '+' | operator == '-':
-        return 1
-    elif operator == '*' | operator == '/':
-        return 2
-    elif operator == '^':
-        return 3
-    elif operator == '(':
-        return 4
-    elif operator == '#':
-        return 0
-def isp(operator):
-    if operator == '+' | operator == '-':
-        return 1
-    elif operator == '*' | operator == '/':
-        return 2
-    elif operator == '^':
-        return 3
-    elif operator == '(':
-        return 0
-    elif operator == '#':
-        return -1
-def convert(infix):
-    s = stack()
-    s.push('#')
-    postfix = ''
-    for ch in infix:
-        if ch.isalnum():
-            postfix += ch
-        elif ch == ')':
-            while s.peek() != '(':
-                postfix += s.pop()
-            s.pop()
-        else:
-            while icp(ch) <= isp(s.peek()):
-                postfix += s.pop()
-            s.push(ch)
-    while s.peek() != '#':
-        postfix += s.pop()
-    return postfix
-infix = input('Enter infix expression : ')
-postfix = convert(infix)
-print('Postfix expression :', postfix)
+from  prog1b  import   stack   
+def  icp(operator):
+	if  operator   in   '+-':
+		return  2  
+	if  operator   in   '*/%':
+		return  3 
+	if  operator  ==  '^':
+		return  4
+	if  operator  ==  ')':
+		return  5
+def  isp(operator):
+	match  operator:
+		case   '+' | '-':
+			return  1
+		case   '*' | '/' | '%':
+			return  2
+		case   '^':
+			return  4
+		case   ')':
+			return  0
+		case   '#':
+			return  -1
+def  convert(infix):
+		infix = infix[::-1]  
+		s = stack()   
+		s . push('#')  
+		prefix = ''  
+		for  ch  in  infix:  
+			if  ch . isdigit():  
+					prefix += ch  
+			elif  ch  ==  '(':
+					while  s . peek()  !=  ')':  
+							prefix  += s.pop() 
+					s . pop() 
+			else:
+					while  icp(ch) <= isp(s . peek()):  
+							prefix += s . pop()  
+					s . push(ch)  
+		while  s . peek() !=  '#':  
+				prefix += s . pop()  
+		return  prefix[::-1]  
+infix = input('Enter Infix expression : ')
+prefix = convert(infix)
+print('Prefix expression : ' ,  prefix)
