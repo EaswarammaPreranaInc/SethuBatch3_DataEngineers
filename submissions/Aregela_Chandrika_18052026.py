376964d1c192385cb624861ed093a72766625d7f
Aregela Naga Chandrika <131333970+ChandrikaAregela@users.noreply.github.com>
2026-05-18 23:57:39 +0530

    Add files via upload

---
+++ submissions/Aregela_Chandrika_18052026.py
@@ -0,0 +1,38 @@
+
+# Modify  the  following  program  with  writerows()  method
+import   csv
+def   create(f):
+	w = csv . writer(f)
+	data = []
+	data.append(['EMP NO' , 'EMP NAME' , 'SALARY'])
+	n = eval(input('How Many Employees ? :   '))   #   3
+	for  i  in  range(n):  #  Iteration   3
+		empno = eval(input('Enter Employee No : '))
+		ename = input('Enter Employee Name : ')
+		sal = eval(input('Enter Employee Salary : '))
+		data.append([empno , ename, sal])
+		w.writerows(data)
+	# End  of  for  loop
+	print(F'File  {f . name}  is  created')
+# End  of  the  function
+fname = input('Enter   filename  :   ')  #   emp.csv
+f = open(fname , 'w' , newline = '')
+create(f)
+f . close()
+
+
+
+
+
+# Write  a  program  to  print  csv  file
+import  csv
+def  disp(f):
+	data = csv.reader(f)  #How  to  itertae  thru  csv  file  using  reader  object
+# End  of  function
+try:
+	fname = input("Enter CSV filename: ")#How  to  read  the  filename
+	f = open(fname,"r")#How  to  open  the  file
+	disp(f) #How  to  print  the  file
+	f.close()  #How  to  close  the  file	
+except  FileNotFoundError:
+	print(F'File  {fname}  does  not  exist')
