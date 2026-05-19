9fe37aa565aef2759979d5e6f26cdad13a7e3ef2
SunkariRakesh <167796150+SunkariRakesh@users.noreply.github.com>
2026-05-19 10:11:17 +0530

    Add files via upload

---
+++ submissions/Sunkari_Rakesh_20260518.py
@@ -0,0 +1,31 @@
+
+import   csv
+def display(f):
+	r=csv.reader(f)
+	while True:
+		try:
+			row=next(r)
+			print(f"{row[0]:^10} {row[1]:^15} {row[2]:^10}")
+		except:
+			break
+	
+def   create(f):
+	w = csv . writer(f)
+	w . writerow(['EMP NO' , 'EMP NAME' , 'SALARY'])
+	data=[]
+	n = eval(input('How Many Employees ? :   '))   #   3
+	for  i  in  range(n):  #  Iteration   3
+		empno = eval(input('Enter Employee No : '))
+		ename = input('Enter Employee Name : ')
+		sal = eval(input('Enter Employee Salary : '))
+		data.append([empno,ename,sal])
+	w . writerows(data)
+	# End  of  for  loop
+	print(F'File  {f . name}  is  created')
+# End  of  the  function
+fname = input('Enter   filename  :   ')  #   emp.csv
+f = open(fname , 'w' , newline = '')
+create(f)
+f . close()
+f = open(fname , 'r' )
+display(f)
