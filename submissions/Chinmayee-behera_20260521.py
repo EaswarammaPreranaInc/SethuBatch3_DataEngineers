02f567f6cd3bbcd37d8262fa5c2a8409259db42a
Chinmayee Behera <chinmayeebehera768@gmail.com>
2026-05-22 08:20:06 +0530

    Create Chinmayee-behera_20260521.py

---
+++ submissions/Chinmayee-behera_20260521.py
@@ -0,0 +1,538 @@
+# Find  outputs   (Home  work)
+try:
+	print('Outer   try')
+	try:
+		print('Inner    try')
+		print(7 / 0)   --> Raises ZeroDivisonError
+		int('Hyd') # Skipped
+		'Hyd'[5] # Skipped
+		eval('Hyd') # Skipped
+	except   ZeroDivisionError:
+		print('ZDE   of   inner   try')
+		int('Ten') 
+	except  ValueError: # Skipped
+		print('ValueError  of  inner  try')
+	finally:
+		print('Inner  try  finally')
+	print('End  of  inner  try') # Skipped
+except   ValueError:
+	print('ValueError  of  outer  try')
+except   IndexError: # Skipped
+	print('IndexError  of  outer  try')
+except: # Skipped
+	print('default  except  of  outer  try')
+finally:
+	print('Outer  try  finally')
+print('End  of  outer  try')
+'''
+Outer try
+Inner try
+ZDE of inner try
+Inner try finally
+ValueError of outer try
+Outer try finally
+End of outer try
+'''
+
+#  Find outputs   (Home  work)
+try:
+	print('Outer  try')
+	try:
+		print('Inner  try')
+		int('Hyd')  --> Raises ValueError
+		'Hyd'[5] # Skipped
+		eval('Hyd') # Skipped
+	except  ZeroDivisionError: # Skipped
+		print('ZDE  of  inner  try')
+		int('Ten') # Skipped
+	except  ValueError:
+		print('ValueError  of  inner  try ')
+	finally:
+		print('Inner  try  finally')
+	print('End  of  inner  try')
+except  ValueError: # Skipped
+	print('ValueError  of  outer try')
+except  IndexError: # Skipped
+	print('IndexError of outer try')
+except: # Skipped
+	print('default except of outer try')
+finally:
+	print('Outer try finally')
+print('End of outer try')
+'''
+Outer try
+Inner try
+ValueError  of  inner  try 
+Inner  try  finally
+End  of  inner  try
+Outer try finally
+End of outer try
+'''
+
+#  Find outputs   (Home  work)
+try:
+	print('Outer  try')
+	try:
+		print('Inner  try')
+		'Hyd'[3]  --> Raises IndexError
+		eval('Hyd') # Skipped
+	except  ZeroDivisionError: # Skipped
+		print('ZDE  of  inner  try')
+		int('Ten') # Skipped
+	except  ValueError: # Skipped
+		print('ValueError  of  inner  try ')
+	finally:
+		print('Inner  try  finally')
+	print('End  of  inner  try')
+except  ValueError: # Skipped
+	print('ValueError  of  outer  try')
+except  IndexError:
+	print('IndexError  of  outer  try')
+except: # Skipped
+	print('default except of outer try')
+finally:
+	print('Outer try finally')
+print('End  of  outer  try')
+'''
+Outer  try
+Inner  try
+Inner  try  finally
+IndexError  of  outer  try
+Outer try finally
+End  of  outer  try
+'''
+
+#  Find  outputs (Home  work)
+try:
+	print('Outer  try')
+	try:
+		print('Inner  try')
+		eval('Hyd') --> Raises NameError
+	except  ZeroDivisionError: # Skipped
+		print('ZDE  of  inner  try')
+		int('Ten') # Skipped
+	except ValueError: # Skipped
+		print('ValueError  of   inner  try ')
+	finally:
+		print('Inner  try  finally')
+	print('End of inner try') # Skipped
+except  ValueError: # Skipped
+	print('ValueError  of  outer try')
+except  IndexError: # Skipped
+	print('IndexError of outer try')
+except:
+	print('default  except  of  outer  try')
+finally:
+	print('Outer  try  finally')
+print('End  of  outer  try')
+'''
+Outer  try
+Inner  try
+Inner  try  finally
+default  except  of  outer  try
+Outer  try  finally
+End  of  outer  try
+'''
+
+#  Find  outputs (Home  work)
+try:
+	print('Outer  try')
+	try:
+		print('Inner  try')
+		print(10 + '20')  --> Raises TypeError
+	except  ZeroDivisionError: # Skipped
+		print('ZDE  of  inner  try')
+		int('Ten') # Skipped
+	except ValueError: # Skipped
+		print('ValueError  of   inner  try ')
+	finally:
+		print('Inner  try  finally')
+	print('End of inner try') # Skipped
+except  ValueError: # Skipped
+	print('ValueError  of  outer try')
+except  IndexError: # Skipped
+	print('IndexError of outer try')
+finally:
+	print('Outer  try  finally')
+print('End  of  outer  try') # Skipped
+'''
+Outer  try
+Inner  try
+Inner  try  finally
+Outer  try  finally
+TypeError Reported
+'''
+
+# Find  outputs   (Home  work)
+class   MyError:
+	def   __init__(self , y):
+		self . a = y
+		print('Constructor')
+# End  of  the  class
+def  compute(x):
+		print(x)  
+		if  x > 20:
+			raise   MyError(x)  
+		print('Hello') 
+# End  of  the function
+try:
+	compute(10)
+	compute(30)
+except  MyError  as  msg:
+	print('Caught  MyError  outside  :  ' ,  msg)
+print('End')
+'''
+10
+Hello
+30
+Constructor
+# Can't raise MyError becoz it's not child of Exception or BaseException
+'''
+
+# Find  outputs   (Home  work)
+class   MyError(NameError):
+	def    __init__(self):
+		self . a =  25
+		print('Constructor')
+# End of  the class
+def  compute(x):
+	print(x)
+	if  x > 20:
+		raise   MyError()
+	print('Hello') # Skipped
+# End  of  the  function
+try:
+	compute(30) 
+	compute(10) # Skipped
+except  MyError  as  msg:
+	print('Caught  MyError  outside  :  ' ,  msg)
+print('End')
+'''
+30
+Constructor
+Caught  MyError  outside  :  
+End
+'''
+
+# Find  outputs (Home  work)
+try:
+	print(1)
+	print(2)
+	print(3)
+except: # Skipped
+	print(4) 
+else:
+	print(5)
+finally:
+	print(6)
+print(7)
+'''
+1
+2
+3
+5
+6
+7
+'''
+
+# Find  outputs   (Home  work)
+try:
+	print(1)
+	print(7 / 0) 
+	print(3) # Skipped
+except:
+	print(4)
+else:
+	print(5) # Skipped
+finally:
+	print(6)
+print(7)
+'''
+1
+4
+6
+7
+'''
+
+# Find  outputs   (Home  work)
+try:
+	print(1) 
+	print(7 / 0) 
+	print(3) # Skipped
+except:
+	int('Two')  
+else:
+        print(5) # Skipped
+finally:
+        print(6)
+print(7) # Skipped
+'''
+1
+6
+ZeroDivisonError Reported
+ValueError Reported
+'''
+
+# Find  outputs  (Home  work)
+from  threading  import  *
+class  c1:
+	def  m1(self):
+		for  i  in  range(10):
+			print('child  thread')
+a = c1()
+child  = Thread(target = a . m1)
+a . m1()
+for  i  in  range(10):
+	print('main  thread')
+'''
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+'''
+
+# Find  outputs (Home  work)
+from  threading  import   *
+class   c1:
+	def  m1(self):
+		for  i  in  range(10):
+			print('child  thread')
+a = c1()
+child = Thread(target =  a . m1())
+child . start()
+for  i  in  range(10):
+        print('main  thread')
+'''
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+'''
+
+#  Find  outputs  (Home  work)
+from  threading  import  *
+class  c1:
+	@classmethod
+	def  m1(cls):
+		for  i   in  range(1 , 11):
+			print('Child  Thread  :  ' , i)
+child = Thread(target = c1.m1)
+child . start()
+for  i  in  range(1 , 11):
+        print('Main  Thread  :  ' , i)
+'''
+Child  Thread  :   1
+Main  Thread  :   1
+Child  Thread  :   2
+Main  Thread  :   2
+Main  Thread  :   3
+Main  Thread  :   4
+Main  Thread  :   5
+Main  Thread  :   6
+Main  Thread  :   7
+Main  Thread  :   8
+Main  Thread  :   9
+Main  Thread  :   10
+Child  Thread  :   3
+Child  Thread  :   4
+Child  Thread  :   5
+Child  Thread  :   6
+Child  Thread  :   7
+Child  Thread  :   8
+Child  Thread  :   9
+Child  Thread  :   10
+'''
+
+# Find  outputs  (Home  work)
+from  threading  import  Thread
+class   Thread:
+        def   run(self):
+                for  i  in  range(10):
+                        print('Child  Thread')
+# End of the class
+t = Thread()
+t . start() # ERROR because object 't' has no start method and is overridden when imported
+for  i  in  range(10):
+        print('main  thread')
+
+# Find  outputs  (Home  work)
+class   Thread:
+        def   run(self):
+                for  i  in  range(10):
+                        print('Child  Thread')
+from  threading  import  Thread
+t = Thread()
+t . start()
+for  i  in  range(10):
+        print('Main  Thread')
+'''
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+'''
+
+# Find  outputs  (Home  work)
+from threading import *
+class    MyThread(Thread):
+        def   run(self):
+                for  i  in  range(10):
+                        print('child  thread')
+# End  of  the  class
+child = MyThread()
+child .  run()
+for  i  in  range(10):
+        print('main  thread')
+'''
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+child  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+main  thread
+'''
+
+# Find  outputs
+from  threading  import  *
+class   MyThread(Thread):
+	def   run(self):
+			print('run  method')
+def  f1():
+	print('f1  function')
+new = MyThread(target = f1)
+new . start()
+print('Main  Thread')
+'''
+run  method
+Main  Thread
+'''
+
+# Find  outputs
+from  threading  import  *
+class   MyThread(Thread):
+	pass
+def  f1():
+	for  i  in   range(10):
+		print('f1  function')
+new = MyThread(target = f1) 
+new . start()   
+for  i  in  range(10):
+	print('Main  Thread')
+'''
+f1  function
+f1  function
+f1  function
+f1  function
+f1  function
+f1  function
+f1  function
+f1  function
+f1  function
+f1  function
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+'''
+
+# Find  outputs
+from  threading  import  *
+class   MyThread(Thread):
+	pass
+new = MyThread() 
+new . start()  
+for  i  in  range(10):
+	print('Main  Thread')
+'''
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+'''
+
+# Find  outputs (Home  work)
+from  threading  import *
+class  MyThread(Thread):
+	def   walk(self):
+		for  i  in  range(10):
+			print('walk  method')
+child = MyThread()
+child . start()
+for  i  in  range(10):
+	print('Main  Thread')
+'''
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+Main  Thread
+'''	
